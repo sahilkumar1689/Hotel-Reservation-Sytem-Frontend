75d169fd524d8af0941615eab8be6820d45f25b8
Sahil Kumar <Sahilkumar32755@gmail.com>
2024-05-28 14:14:29 +0530

    Intial Commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SAHIL KUMAR</div>
+          <div className="right">
+            <p>Z6 Gulshan-e-Maymar, Jalandhar</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Sahil Kumar</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Sahil Kumar.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
