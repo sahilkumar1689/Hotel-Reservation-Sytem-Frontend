e8c25ea8fcdfc02a443f53da403d33e3ef49bfa9
Sahil Kumar <Sahilkumar32755@gmail.com>
2024-06-06 16:46:47 +0530

    Cores error fixes

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://tempbackend-uke1.onrender.com/api/v1/reservation/send",
+        "/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
